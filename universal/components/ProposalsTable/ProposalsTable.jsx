63fe4d75a2c8752effa81bab74b1b64f19615e96
Gabriel Bonner <gabrielbonner@gmail.com>
2016-08-15 19:09:34 -0700

    Render row info for a test proposal

--- universal/components/ProposalsTable/ProposalsTable.jsx
+++ universal/components/ProposalsTable/ProposalsTable.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
+
 const ProposalsTable = React.createClass({
-  render() {
+  render: function() {
+
+    var proposals = [
+      { id: 1, speakerId: 1, talkId: 1 }
+    ];
+
+    var proposalRows = proposals.map(function(proposal) {
+      return React.createElement(ProposalRow, { proposal: proposal });
+    });
+
     return (
       <div>
         <div>
@@ -9,31 +19,32 @@ const ProposalsTable = React.createClass({
           <button>Import</button>
           <button>Export</button>
         </div>
-      <div>
-      <table>
-      <thead>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>Proposal Id</th>
+                <th>Speaker Id</th>
+                <th>Talk Id</th>
+              </tr>
+            </thead>
+            <tbody>
+              {proposalRows}
+            </tbody>
+          </table>
+        </div>
+      </div>)
+  }
+});
+
+var ProposalRow = React.createClass({
+  render: function() {
+    return (
       <tr>
-      <th>#</th>
-    <th>First Name</th>
-    <th>Last Name</th>
-    <th>Username</th>
-  </tr>
-  </thead>
-    <tbody>
-      <tr>
-        <td>1</td>
-        <td>Mark</td>
-        <td>Tompson</td>
-        <td>the_mark7</td>
-        <td>
-          <a href="user.html"><i className="icon-pencil"></i></a>
-          <a href="#myModal" role="button" data-toggle="modal"><i className="icon-remove"></i></a>
-        </td>
-      </tr>
-    </tbody>
-    </table>
-    </div>
-    </div>)
+        <td>{this.props.proposal.id}</td>
+        <td>{this.props.proposal.speakerId}</td>
+        <td>{this.props.proposal.talkId}</td>
+      </tr>)
   }
 });
 
